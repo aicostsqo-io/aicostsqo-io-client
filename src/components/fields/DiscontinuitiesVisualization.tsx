1d568ab07ac4c6c9dfc910568b5c1b8e08eb1026
mustafahincal <mustafahncal@gmail.com>
2023-10-28 17:13:36 +0300

    refactor: disc visualization

--- src/components/fields/DiscontinuitiesVisualization.tsx
+++ src/components/fields/DiscontinuitiesVisualization.tsx
@@ -4,42 +4,70 @@ import * as M from "marble-disc";
 import { getDiscsByRpId } from "@/api/disc";
 
 const DiscontinuitiesVisualization = () => {
-  const { selectedRP, selectedDiscs, setSelectedDiscs } = useSiteContext();
+  const { selectedRP } = useSiteContext();
   const marbleRef = useRef(null);
+  const [clearFunction, setClearFunction] = React.useState<any>(null);
+  const [discontinuities, setDiscontinuities] = React.useState<any>(null);
+  const [loading, setLoading] = React.useState<boolean>(false);
+
+  console.log("selectedRP : ", selectedRP);
+
   useEffect(() => {
-    if (!!selectedDiscs && selectedDiscs.length > 0) {
+    if (!selectedRP) return;
+    getDiscsByRpId(selectedRP?._id)
+      .then((res) => {
+        setDiscontinuities(res.data.discs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return () => {
+      marbleRef.current = null;
+      console.log(clearFunction);
+      if (clearFunction) {
+        console.log("clearing function");
+        clearFunction();
+      }
+      console.warn("test çıkıldı ve temizlendi");
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!!discontinuities && discontinuities.length > 0) {
+      setLoading(true);
       const mrbl = new M.Marble(
         marbleRef.current,
         selectedRP.sizeX,
         selectedRP.sizeY,
         selectedRP.sizeZ
       );
-      mrbl.showDiscontinuities(selectedDiscs);
+
+      const showAndHideDiscontinuities = async () => {
+        try {
+          console.log("mrbl : ", mrbl.showDiscontinuities);
+          await mrbl.showDiscontinuities(discontinuities);
+          console.log("test");
+          setLoading(false);
+        } catch (error) {
+          // Hata yönetimi
+          console.error("Hata: ", error);
+        }
+      };
+
+      showAndHideDiscontinuities();
+
+      setClearFunction(() => mrbl.clear);
     }
-  }, [selectedRP, selectedDiscs]);
+  }, [discontinuities]);
 
-  const fetchDiscData = async () => {
-    const res = await getDiscsByRpId(selectedRP?._id);
-    setSelectedDiscs(res.data.discs);
-  };
-
-  useEffect(() => {
-    fetchDiscData();
-  }, []);
-
-  useEffect(() => {
-    return () => {
-      marbleRef.current = null;
-      console.warn("çıkıldı ve temizlendi");
-    };
-  }, []);
-
-  if (selectedDiscs?.length === 0) return <div>No Disc</div>;
+  if (discontinuities?.length === 0) return <div>No Disc</div>;
 
   return (
     <div className="flex flex-row h-full">
       <div className="w-full flex justify-center items-center">
         <div className="w-full h-full">
+          {loading && <div className="text-3xl font-bold">Loading...</div>}
           <div className="w-full h-full" ref={marbleRef}></div>
         </div>
       </div>
