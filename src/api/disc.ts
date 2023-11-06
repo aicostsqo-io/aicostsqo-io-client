498ce082a4727abe9dc9e8b1645a1f1269ff4bd1
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2023-11-06 16:16:59 +0300

    refactor rp disc endpoint

--- src/api/disc.ts
+++ src/api/disc.ts
@@ -1,6 +1,6 @@
 import { instance as axios } from "./axiosInstance";
 
-export const getDiscsByRpId = (id: any) => axios.get(`/discs/${id}`);
-export const createDisc = (data: any) => axios.post("/discs", data);
+export const getDiscsByRpId = (id: any) => axios.get(`/rp-discs/${id}`);
+export const createDisc = (data: any) => axios.post("/rp-discs", data);
 export const bulkDeleteRpDiscs = (data: string[]) =>
-  axios.post("/discs/bulk-delete", data);
+  axios.post("/rp-discs/bulk-delete", data);
