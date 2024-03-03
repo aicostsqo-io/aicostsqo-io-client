7a152f4d5f347ad9abb90fd5833ae5cea0f9da0a
mustafahincal <mustafahncal@gmail.com>
2024-03-03 21:04:37 +0300

    feat: site generating things part inputs

--- src/components/sidebar/Tree.tsx
+++ src/components/sidebar/Tree.tsx
@@ -12,9 +12,10 @@ import { useTreeContext } from "@/contexts/Tree";
 import { useSiteContext } from "@/contexts/Site";
 import { toast } from "react-toastify";
 
-const RPTreeItem = ({ rps, setPoint, index }: any) => {
+const RPTreeItem = ({ rps, setPoint, site, index }: any) => {
   const { setSelectedRP, setSelectedRPs, setSelectedDiscs } = useSiteContext();
   const { expanded, setExpanded } = useTreeContext();
+  const { calculatePolyhedrons, generateVirtual1DExtendedRPs } = site;
 
   const handleClickRpTreeItem = (rp: any) => {
     setPoint("RPItem");
@@ -60,20 +61,25 @@ const RPTreeItem = ({ rps, setPoint, index }: any) => {
               // setSelectedDiscs([]);
             }}
           />
-          <TreeItem
-            nodeId={rp?._id + 3}
-            label={"Polyhedron"}
-            onClick={() => {
-              setPoint("Polyhedron");
-            }}
-          />
-          <TreeItem
-            nodeId={rp?._id + 4}
-            label={"Extended"}
-            onClick={() => {
-              setPoint("Extended");
-            }}
-          />
+          {calculatePolyhedrons ? (
+            <TreeItem
+              nodeId={rp?._id + 3}
+              label={"Polyhedron"}
+              onClick={() => {
+                setPoint("Polyhedron");
+              }}
+            />
+          ) : null}
+          {generateVirtual1DExtendedRPs ? (
+            <TreeItem
+              nodeId={rp?._id + 4}
+              label={"Extended"}
+              onClick={() => {
+                setPoint("Extended");
+              }}
+            />
+          ) : null}
+
           {/* {
               field?.rps?.discs?.length > 0 ? <DiscTreeItem discs={field?.rps?.discs} /> : <TreeItem nodeId={"199"} label={"No Discs"} /> 
             } */}
@@ -97,6 +103,7 @@ const FieldTreeItem = ({ field, router, setPoint, index }: any) => {
     setSelectedSite(field);
     router.push(`/project/fields/${field?.site?._id}`);
   };
+
   return (
     <TreeItem
       nodeId={"Site" + field?.site?._id}
@@ -150,7 +157,12 @@ const FieldTreeItem = ({ field, router, setPoint, index }: any) => {
         }}
       />
       {field?.rps?.length > 0 ? (
-        <RPTreeItem setPoint={setPoint} rps={field?.rps} index={index} />
+        <RPTreeItem
+          setPoint={setPoint}
+          site={field?.site}
+          rps={field?.rps}
+          index={index}
+        />
       ) : (
         <TreeItem nodeId={"No RPs"} label={"No RPs"} />
       )}
